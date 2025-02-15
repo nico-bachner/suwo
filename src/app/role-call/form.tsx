0d1cc6bad69dec30c477cf912fc8f3438831acf9
Nico Bachner <git@nbac.me>
2025-02-15 11:55:25 +1100

    fix: show all members

---
+++ src/app/role-call/form.tsx
@@ -0,0 +1,62 @@
+import { Switch } from 'radix-ui'
+
+import { RoleCall, Table } from '@/db'
+import { getQueryBuilder } from '@/neon'
+
+type FormProps = {
+  year: number
+  semester: number
+  week: number
+  id?: number
+  name?: string
+}
+
+export const Form = async ({ year, semester, week, id, name }: FormProps) => {
+  const sql = getQueryBuilder()
+  const role_call: Table<RoleCall> =
+    await sql`SELECT present FROM role_call WHERE year = ${year} AND semester = ${semester} AND week = ${week} and member = ${id}`
+
+  return (
+    <form
+      key={id}
+      action={async (formData: FormData) => {
+        'use server'
+        const sql = getQueryBuilder()
+
+        const role_call: Table<RoleCall> =
+          await sql`SELECT member FROM role_call WHERE year = ${year} AND semester = ${semester} AND week = ${week} and member = ${id}`
+
+        if (role_call.length > 0) {
+          console.log('updating')
+          await sql`
+                  UPDATE role_call
+                  SET present = ${formData.get('present') == 'on'}
+                  WHERE year = ${year} AND semester = ${semester} AND week = ${week} AND member = ${id}
+                `
+        } else {
+          console.log('inserting')
+          await sql`INSERT INTO role_call VALUES (${year}, ${semester}, ${week}, ${id}, ${formData.get('present') == 'on'})`
+        }
+      }}
+      className="flex flex-row odd:bg-gray-800"
+    >
+      <div className="flex flex-1 flex-row items-center justify-between gap-2 px-4">
+        <span className="flex-1">{name}</span>
+
+        <span>Not Present</span>
+        <Switch.Root
+          name="present"
+          defaultChecked={role_call[0]?.present ?? false}
+          className="flex w-10 flex-row rounded-full border border-gray-500 bg-gray-950 p-1 data-[state=checked]:justify-end"
+        >
+          <Switch.Thumb className="data-[state=checked]:bg-white500 block h-4 w-4 rounded-full bg-gray-500" />
+        </Switch.Root>
+        <span>Present</span>
+      </div>
+
+      <button type="submit" className="bg-gray-500 px-4 py-2">
+        Confirm
+      </button>
+    </form>
+  )
+}
--- src/app/role-call/page.tsx
+++ src/app/role-call/page.tsx
@@ -1,7 +1,7 @@
-import { Switch } from 'radix-ui'
-
-import { Member, RoleCall, Table } from '@/db'
+import { Member, Table } from '@/db'
 import { getQueryBuilder } from '@/neon'
+
+import { Form } from './form'
 
 type PageProps = {
   searchParams: Promise<{
@@ -27,8 +27,7 @@ export default async function Page({ searchParams }: PageProps) {
   }
 
   const sql = getQueryBuilder()
-  const members: Table<Member & RoleCall> =
-    await sql`SELECT * FROM members LEFT JOIN role_call ON members.id = role_call.member WHERE year = ${year} AND semester = ${semester} AND week = ${week}`
+  const members: Table<Member> = await sql`SELECT * FROM members`
 
   return (
     <main className="flex flex-col items-center gap-6 p-6">
@@ -37,48 +36,15 @@ export default async function Page({ searchParams }: PageProps) {
       </h1>
 
       <div className="flex w-full max-w-screen-sm flex-col">
-        {members.map(({ id, name, present }) => (
-          <form
+        {members.map(({ id, name }) => (
+          <Form
             key={id}
-            action={async (formData: FormData) => {
-              'use server'
-              const sql = getQueryBuilder()
-
-              const role_call: Table<RoleCall> =
-                await sql`SELECT member FROM role_call WHERE year = ${year} AND semester = ${semester} AND week = ${week} and member = ${id}`
-
-              if (role_call.length > 0) {
-                console.log('updating')
-                await sql`
-                  UPDATE role_call
-                  SET present = ${formData.get('present') == 'on'}
-                  WHERE year = ${year} AND semester = ${semester} AND week = ${week} AND member = ${id}
-                `
-              } else {
-                console.log('inserting')
-                await sql`INSERT INTO role_call VALUES (${year}, ${semester}, ${week}, ${id}, ${formData.get('present') == 'on'})`
-              }
-            }}
-            className="flex flex-row odd:bg-gray-800"
-          >
-            <div className="flex flex-1 flex-row items-center justify-between gap-2 px-4">
-              <span className="flex-1">{name}</span>
-
-              <span>Not Present</span>
-              <Switch.Root
-                name="present"
-                defaultChecked={present}
-                className="flex w-10 flex-row rounded-full border border-gray-500 bg-gray-950 p-1 data-[state=checked]:justify-end"
-              >
-                <Switch.Thumb className="data-[state=checked]:bg-white500 block h-4 w-4 rounded-full bg-gray-500" />
-              </Switch.Root>
-              <span>Present</span>
-            </div>
-
-            <button type="submit" className="bg-gray-500 px-4 py-2">
-              Confirm
-            </button>
-          </form>
+            year={parseInt(year)}
+            semester={parseInt(semester)}
+            week={parseInt(week)}
+            id={id}
+            name={name}
+          />
         ))}
       </div>
     </main>
