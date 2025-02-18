c76b27c8e2fd12c37d1846ae36ba25e6adad71d6
oskarmatre <69513601+oskarmatre@users.noreply.github.com>
2025-02-18 15:16:27 +1100

    feat: QR code for roll call

--- src/app/roll-call/page.tsx
+++ src/app/roll-call/page.tsx
@@ -1,5 +1,7 @@
 import { CheckIcon } from '@heroicons/react/24/outline'
 import { Checkbox } from 'radix-ui'
+import { Dialog } from 'radix-ui'
+import QRCode from 'react-qr-code'
 
 import { Submit } from '@/components/client/submit'
 import { TextInput } from '@/components/ui/text_input'
@@ -34,8 +36,32 @@ export default async function Page({ searchParams }: PageProps) {
   const members: Table<Member> = await sql`SELECT * FROM members`
 
   return (
-    <main className="prose flex w-full flex-col items-center">
+    <main className="prose flex w-full flex-col items-center gap-1">
       <h1>Roll Call – Week {week}</h1>
+      <Dialog.Root>
+        <Dialog.Trigger asChild>
+          <button className="cursor-pointer rounded bg-gray-800 px-4 py-2 transition-colors hover:bg-gray-700">
+            Show QR Code
+          </button>
+        </Dialog.Trigger>
+        <Dialog.Overlay className="fixed inset-0 bg-black opacity-50" />
+        <Dialog.Content className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transform rounded bg-gray-800 p-4">
+          <Dialog.Title>QR Code</Dialog.Title>
+          <Dialog.Description>
+            QR code for this week's roll call
+          </Dialog.Description>
+          <QRCode
+            value={
+              'https://suwo.vercel.app/roll-call?year=' +
+              year +
+              '&semester=' +
+              semester +
+              '&week=' +
+              week
+            }
+          />
+        </Dialog.Content>
+      </Dialog.Root>
 
       <div className="flex w-full max-w-screen-sm flex-col">
         {members.map(({ id, ...member }) => (
