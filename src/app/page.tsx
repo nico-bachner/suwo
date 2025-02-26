f9991d571a47eaf0268f083da679f54ce7dd08de
Nico Bachner <git@nbac.me>
2025-02-26 11:32:55 +1100

    fix: remove O week CTA

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-import Image from 'next/image'
-
-import O_week from '@/images/O_week.png'
-
 export default function Page() {
   return (
     <main className="mx-auto flex w-full max-w-screen-md flex-1 flex-col items-stretch justify-center gap-12 sm:gap-20">
@@ -11,32 +7,12 @@ export default function Page() {
         Wind Orchestra
       </h1>
 
-      <div className="grid gap-8 sm:grid-cols-3">
-        <div className="flex flex-col items-center gap-4 sm:items-start">
-          <p className="text-lg sm:text-xl">Come see us at the O-week stand!</p>
-
-          <a
-            href="https://usu.edu.au/clubs/wind-orchestra/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="rounded-lg bg-gradient-to-br from-amber-700 to-yellow-700 px-4 py-2 text-lg font-semibold"
-          >
-            Join via USU
-          </a>
-        </div>
-
-        <Image
-          src={O_week}
-          alt="The SUWO logo"
-          className="h-auto w-full self-center sm:col-span-2 sm:col-start-1 sm:row-start-1"
-        />
-      </div>
-
       <div className="flex flex-col gap-4">
         <iframe
           src="https://calendar.google.com/calendar/embed?height=600&wkst=2&ctz=Australia%2FSydney&hl=en_GB&mode=AGENDA&src=ZDM0ODM2MmRmMGQ4MWIxOTAxYjQxYTMxMWRlMmJiZWFmZmM4ZmJhOGFhMjdiN2MxMDEzMjgxZGIyNzZkY2NkNEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23D81B60"
           className="h-[400px] w-full rounded-lg"
         />
+
         <a
           href="https://calendar.google.com/calendar/u/0?cid=ZDM0ODM2MmRmMGQ4MWIxOTAxYjQxYTMxMWRlMmJiZWFmZmM4ZmJhOGFhMjdiN2MxMDEzMjgxZGIyNzZkY2NkNEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t"
           target="_blank"
