d73536e84b5c756360d6de3e3d947adb3a9bde42
Nico Bachner <git@nbac.me>
2025-02-28 21:32:44 +1100

    feat: Notion content

--- src/app/history/[year]/page.tsx
+++ src/app/history/[year]/page.tsx
@@ -1,3 +1,4 @@
+import { Client, isFullBlock } from '@notionhq/client'
 import { notFound } from 'next/navigation'
 
 import { Params } from '@/types/next'
@@ -12,24 +13,56 @@ type PageProps = {
 
 export default async function Page({ params }: PageProps) {
   const data = await getHistory()
-  const { year: yearParam } = await params
+  const { year } = await params
 
-  const year = data.find(
+  const page = data.find(
     ({ properties }) =>
       properties['Year'].type == 'number' &&
-      properties['Year'].number == parseInt(yearParam),
+      properties['Year'].number == parseInt(year),
   )
 
-  if (!year) {
+  if (!page) {
     return notFound()
   }
+
+  const { blocks } = new Client({
+    auth: process.env.NOTION_TOKEN,
+  })
+
+  const { results } = await blocks.children.list({
+    block_id: page.id,
+  })
 
   return (
     <main className="prose">
       <h1>
-        {year.properties['Year'].type == 'number' &&
-          year.properties['Year'].number}
+        {page.properties['Year'].type == 'number' &&
+          page.properties['Year'].number}
       </h1>
+      {results.map((block) => {
+        if (!isFullBlock(block)) {
+          return null
+        }
+
+        switch (block.type) {
+          case 'heading_1':
+            return (
+              <h2 key={block.id}>{block.heading_1.rich_text[0].plain_text}</h2>
+            )
+          case 'heading_2':
+            return (
+              <h3 key={block.id}>{block.heading_2.rich_text[0].plain_text}</h3>
+            )
+          case 'paragraph':
+            if (block.paragraph.rich_text.length > 0) {
+              return (
+                <p key={block.id}>{block.paragraph.rich_text[0].plain_text}</p>
+              )
+            }
+          default:
+            return null
+        }
+      })}
     </main>
   )
 }
