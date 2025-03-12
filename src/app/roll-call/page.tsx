e53084940b91a752f70c604eada6778827ea6fb4
Nico Bachner <git@nbac.me>
2025-03-12 12:41:50 +1100

    refactor: fallback ui in case usyd api breaks

--- src/app/roll-call/page.tsx
+++ src/app/roll-call/page.tsx
@@ -16,10 +16,11 @@ export default async function Page() {
   const semester = getCurrentSemester()
   const week = await getCurrentWeek()
 
-  if (semester >= 1 && semester <= 2 && week >= 1 && week <= MAX_WEEK) {
+  if (week >= 1 && week <= MAX_WEEK) {
     return redirect(`/roll-call/${year}/${semester}/${week}`)
   }
 
+  // Fallback ui in case the USYD api goes down or changes structure
   return (
     <main className="prose">
       <h1>Roll Call</h1>
@@ -27,15 +28,17 @@ export default async function Page() {
       <p>Please select a week:</p>
 
       <div className="mx-auto flex w-full max-w-screen-sm flex-row flex-wrap gap-1">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].map((i) => (
-          <Link
-            key={i}
-            href={`/roll-call?year=${year}&semester=${semester}&week=${i}`}
-            className="flex h-16 w-16 items-center justify-center rounded-md bg-gray-900 text-xl hover:bg-gray-800"
-          >
-            {i}
-          </Link>
-        ))}
+        {Array.from({ length: MAX_WEEK }, (_, index) => index + 1).map(
+          (week) => (
+            <Link
+              key={week}
+              href={`/roll-call?year=${year}&semester=${semester}&week=${week}`}
+              className="flex h-16 w-16 items-center justify-center rounded-md bg-gray-900 text-xl hover:bg-gray-800"
+            >
+              {week}
+            </Link>
+          ),
+        )}
       </div>
     </main>
   )
