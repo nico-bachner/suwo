80a2d91ea922cb0fbb4d220fa3511fc1db8acb80
Nico Bachner <git@nbac.me>
2025-03-14 18:43:29 +1100

    feat: group members by instrument

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -2,11 +2,21 @@ import Link from 'next/link'
 import { Fragment } from 'react'
 
 import { PageLayout } from '@/components/ui/page_layout'
-import { getMembers } from '@/lib/db/members/get'
+import { getInstruments } from '@/lib/db/instruments/get'
+import { getMembersWithInstruments } from '@/lib/db/members/get'
 import { Instrument, Profile } from '@/lib/db/types'
 
 export default async function Page() {
-  const members = await getMembers()
+  const instruments = await getInstruments()
+  const members = await getMembersWithInstruments()
+
+  const instrumentsByFamily = instruments.reduce(
+    (accInstrumentsByFamily, { name, family }) => ({
+      ...accInstrumentsByFamily,
+      [family]: [...(accInstrumentsByFamily[family] || []), name],
+    }),
+    {} as Record<Instrument['family'], Instrument['name'][]>,
+  )
 
   const membersByInstrument = members.reduce(
     (accMembersByInstrument, { instrument, ...member }) => {
@@ -25,29 +35,41 @@ export default async function Page() {
     {} as Record<Instrument['name'], Profile[]>,
   )
 
+  console.log(instrumentsByFamily)
+  console.log(membersByInstrument)
+
   return (
     <PageLayout title="Members" className="prose">
-      {Object.keys(membersByInstrument).map((instrument) => (
-        <Fragment key={instrument}>
-          <h2>{instrument}</h2>
+      {Object.keys(instrumentsByFamily).map((family) => (
+        <Fragment key={family}>
+          {/* <h2>{family}</h2> Re-add when we have enough members */}
 
-          <div className="grid grid-cols-2 gap-2">
-            {membersByInstrument[instrument].map(
-              ({ id, given_name, family_name, instrument }) => (
-                <Link
-                  key={id}
-                  href={`/members/${id}`}
-                  className="flex flex-col rounded-lg bg-gray-900 px-4 py-2 font-bold"
-                >
-                  <span className="text-gray-300">
-                    {given_name} {family_name}
-                  </span>
+          {instrumentsByFamily[family].map(
+            (instrument) =>
+              membersByInstrument[instrument] && (
+                <Fragment key={instrument}>
+                  <h3>{instrument}</h3>
 
-                  <span className="text-gray-500">{instrument}</span>
-                </Link>
+                  <div className="grid grid-cols-2 gap-2">
+                    {membersByInstrument[instrument].map(
+                      ({ id, given_name, family_name, instrument }) => (
+                        <Link
+                          key={id}
+                          href={`/members/${id}`}
+                          className="flex flex-col rounded-lg bg-gray-900 px-4 py-2 font-bold"
+                        >
+                          <span className="text-gray-300">
+                            {given_name} {family_name}
+                          </span>
+
+                          <span className="text-gray-500">{instrument}</span>
+                        </Link>
+                      ),
+                    )}
+                  </div>
+                </Fragment>
               ),
-            )}
-          </div>
+          )}
         </Fragment>
       ))}
     </PageLayout>
