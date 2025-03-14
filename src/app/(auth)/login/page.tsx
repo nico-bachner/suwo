cdc3ecb8e38ce9cc54adb3589eab4115cd8b87c4
Nico Bachner <git@nbac.me>
2025-03-14 21:25:32 +1100

    fix: improvements to forms

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,11 +1,20 @@
+import Link from 'next/link'
+
 import { PageLayout } from '@/components/ui/page_layout'
 
 import { LoginForm } from './login_form'
 
 export default async function Page() {
   return (
-    <PageLayout title="Log In">
+    <PageLayout title="Log In" className="flex flex-col gap-4">
       <LoginForm />
+
+      <p className="text-right text-sm text-gray-300">
+        Don't have an account?{' '}
+        <Link href="/join" className="font-bold text-blue-500 hover:underline">
+          Sign up instead
+        </Link>
+      </p>
     </PageLayout>
   )
 }
--- src/components/ui/input_label.tsx
+++ src/components/ui/input_label.tsx
@@ -5,7 +5,13 @@ type InputLabelProps = {
 }
 
 export const InputLabel = ({ label, required, ...props }: InputLabelProps) => (
-  <label className="px-2 text-sm text-gray-300 select-none" {...props}>
-    {label} {required && '*'}
+  <label
+    className="flex flex-row justify-between gap-2 px-2 text-sm text-gray-300 select-none"
+    {...props}
+  >
+    <span>{label}</span>
+    <span className="text-xs text-gray-500 uppercase">
+      {required && 'required'}
+    </span>
   </label>
 )
