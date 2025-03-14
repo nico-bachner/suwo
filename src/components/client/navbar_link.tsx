ff8aa80068fd412d704854e199ef808894edecbd
Nico Bachner <git@nbac.me>
2025-03-14 17:27:14 +1100

    refactor: cleanup

--- src/components/client/navbar_link.tsx
+++ src/components/client/navbar_link.tsx
@@ -8,16 +8,9 @@ import { cn } from '@/lib/cn'
 type NavbarLinkProps = {
   children: React.ReactNode
   href: string
-  onClick?: () => void
-  className?: string
 }
 
-export const NavbarLink = ({
-  children,
-  href,
-  onClick,
-  className,
-}: NavbarLinkProps) => {
+export const NavbarLink = ({ children, href }: NavbarLinkProps) => {
   const pathname = usePathname()
 
   const topLevelPath = pathname.split('/')[1]
@@ -26,11 +19,9 @@ export const NavbarLink = ({
   return (
     <Link
       href={href}
-      onClick={onClick}
       className={cn(
         'rounded-lg px-4 py-2 text-lg font-bold text-gray-300 hover:bg-gray-800 hover:text-gray-100',
         topLevelHref == topLevelPath && 'bg-gray-900 text-gray-100',
-        className,
       )}
     >
       {children}
--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,39 @@
+import { FacebookIcon, InstagramIcon, YouTubeIcon } from '@/icons'
+
 export const MAX_WEEK = 13
-export const YOUTUBE = 'https://www.youtube.com/user/SydneyUniWindOrch'
-export const FACEBOOK = 'https://www.facebook.com/sydneyuniversitywindorchestra'
-export const INSTAGRAM = 'https://www.instagram.com/suwo.syd/'
+export const NAV_LINKS = [
+  {
+    href: '/about',
+    label: 'About',
+  },
+  {
+    href: '/history',
+    label: 'History',
+  },
+  {
+    href: '/roll-call',
+    label: 'Roll Call',
+  },
+  {
+    href: '/join',
+    label: 'Join',
+  },
+  {
+    href: `https://calendar.google.com/calendar/u/0?cid=${process.env.GOOGLE_CALENDAR_ID}`,
+    label: 'Calendar',
+  },
+]
+export const SOCIAL_LINKS = [
+  {
+    icon: YouTubeIcon,
+    href: 'https://www.youtube.com/user/SydneyUniWindOrch',
+  },
+  {
+    icon: FacebookIcon,
+    href: 'https://www.facebook.com/sydneyuniversitywindorchestra',
+  },
+  {
+    icon: InstagramIcon,
+    href: 'https://www.instagram.com/suwo.syd/',
+  },
+]
