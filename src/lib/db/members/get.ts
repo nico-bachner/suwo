86c3997ae70d23d43abaf1f7b17f511089e6be6b
Nico Bachner <git@nbac.me>
2025-03-14 18:43:29 +1100

    feat: group members by instrument

--- src/lib/db/members/get.ts
+++ src/lib/db/members/get.ts
@@ -1,5 +1,5 @@
 import { getQueryBuilder } from '@/lib/db/query'
-import { Profile, Table } from '@/lib/db/types'
+import { Instrument, Profile, Table } from '@/lib/db/types'
 
 export const getMembers = async () => {
   const sql = getQueryBuilder()
@@ -11,4 +11,19 @@ export const getMembers = async () => {
   `
 
   return members as Table<Profile>
+}
+
+export const getMembersWithInstruments = async () => {
+  const sql = getQueryBuilder()
+
+  const members = await sql`
+    SELECT id, given_name, family_name, instrument
+    FROM members
+    WHERE instrument IS NOT NULL
+    ORDER BY given_name, family_name
+  `
+
+  return members as Table<
+    Omit<Profile, 'instrument'> & { instrument: Instrument['name'] }
+  >
 }
