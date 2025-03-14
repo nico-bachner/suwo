82d9e5e360c2a4c147e87d2971f60e977d1760fc
Nico Bachner <git@nbac.me>
2025-03-14 17:35:45 +1100

    fix: migrate remaining pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
+import { PageLayout } from '@/components/ui/page_layout'
+
 export default function Page() {
   return (
-    <main className="prose">
-      <h1 className="text-center font-serif">
-        The University of Sydney
-        <br />
-        Wind Orchestra
-      </h1>
-
+    <PageLayout title="The University of Sydney Wind Orchestra">
       <iframe
         src={`https://calendar.google.com/calendar/embed?height=600&wkst=2&ctz=Australia%2FSydney&hl=en_GB&mode=AGENDA&src=${process.env.GOOGLE_CALENDAR_ID}&color=%23D81B60`}
-        className="mx-auto h-full min-h-[360px] w-full max-w-screen-sm rounded-lg"
+        className="h-full min-h-96 w-full rounded-lg"
       />
-    </main>
+    </PageLayout>
   )
 }
