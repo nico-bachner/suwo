5960c26b2ccc9c6a9f7c32f7aea767ea45c6b19e
Nico Bachner <git@nbac.me>
2025-03-14 21:25:32 +1100

    fix: improvements to forms

--- src/app/(auth)/join/page.tsx
+++ src/app/(auth)/join/page.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 import { PageLayout } from '@/components/ui/page_layout'
 import { getInstruments } from '@/lib/db/instruments/get'
 
@@ -7,8 +9,15 @@ export default async function Page() {
   const instruments = await getInstruments()
 
   return (
-    <PageLayout title="Join">
+    <PageLayout title="Join" className="flex flex-col gap-4">
       <CreateMemberForm instruments={instruments} />
+
+      <p className="text-right text-sm text-gray-300">
+        Already have an account?{' '}
+        <Link href="/login" className="font-bold text-blue-500 hover:underline">
+          Log in instead
+        </Link>
+      </p>
     </PageLayout>
   )
 }
