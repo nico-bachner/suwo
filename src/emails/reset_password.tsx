25e123385ac4c5fe59d6e3239fffc0c9dd97fb4d
Nico Bachner <git@nbac.me>
2025-03-16 15:22:17 +1100

    feat: email styling

--- src/emails/reset_password.tsx
+++ src/emails/reset_password.tsx
@@ -1,15 +1,46 @@
+import {
+  Body,
+  Button,
+  Container,
+  Head,
+  Html,
+  Preview,
+  Section,
+  Tailwind,
+  Text,
+} from '@react-email/components'
+
 type ResetPasswordTemplateProps = {
   link: string
 }
 
 export const ResetPasswordTemplate = ({ link }: ResetPasswordTemplateProps) => (
-  <div>
-    <h1>Reset Your Password</h1>
+  <Html>
+    <Head />
 
-    <p>
-      Click the link below to reset your password:
-      <br />
-      <a href={link}>{link}</a>
-    </p>
-  </div>
+    <Tailwind>
+      <Body className="px-2 font-sans">
+        <Preview>Reset your password</Preview>
+
+        <Container>
+          <Section>
+            <Text>
+              Someone recently requested a password change for your SUWO
+              account. If this was you, you can set a new password here:
+            </Text>
+            <Button
+              href={link}
+              className="rounded-lg bg-amber-500 px-4 py-2 text-white"
+            >
+              Reset password
+            </Button>
+            <Text>
+              If you don&apos;t want to change your password or didn&apos;t
+              request this, just ignore and delete this message.
+            </Text>
+          </Section>
+        </Container>
+      </Body>
+    </Tailwind>
+  </Html>
 )
