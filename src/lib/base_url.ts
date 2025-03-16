5f0b0c6d41576224e621e0d98f76880286363faa
Nico Bachner <git@nbac.me>
2025-03-16 15:45:44 +1100

    fix: base url on preview deployments

--- src/lib/base_url.ts
+++ src/lib/base_url.ts
@@ -1,4 +1,13 @@
-export const BASE_URL =
-  process.env.NODE_ENV == 'production'
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-    : 'http://localhost:3000'
+const getBaseURL = () => {
+  switch (process.env.VERCEL_ENV) {
+    case 'production':
+    case 'preview':
+      return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    case 'development':
+      return 'http://localhost:3000'
+    default:
+      throw new Error('Unknown environment')
+  }
+}
+
+export const BASE_URL = getBaseURL()
