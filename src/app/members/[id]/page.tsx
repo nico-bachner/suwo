1b43433edf4cf588fd8c182d294c0440e454038c
Nico Bachner <git@nbac.me>
2025-03-19 17:33:26 +1100

    fix: improve profile edit button colour

--- src/app/members/[id]/page.tsx
+++ src/app/members/[id]/page.tsx
@@ -37,7 +37,7 @@ export default async function Page({ params }: PageProps) {
         session.id == id && (
           <Link
             href={`/members/${id}/edit`}
-            className="flex cursor-pointer flex-row items-center rounded-full bg-amber-900 px-4 py-2 transition-colors select-none hover:bg-amber-800 focus:bg-amber-900 focus:outline-none"
+            className="flex cursor-pointer flex-row items-center rounded-full bg-amber-700 px-4 py-2 transition-colors select-none hover:bg-amber-800 focus:bg-amber-900 focus:outline-none"
           >
             <PencilIcon className="h-5 w-5 stroke-gray-300" />
 
