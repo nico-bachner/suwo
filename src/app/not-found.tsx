72b1e2f2ff60865592509603cdd97a1c73f695cc
Nico Bachner <git@nbac.me>
2025-03-19 16:16:11 +1100

    fix: improve 404 page styling

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,22 @@
 import Link from 'next/link'
 
+import { Button } from '@/components/ui/button'
 import { PageLayout } from '@/components/ui/page_layout'
 
 export default function Page() {
   return (
-    <PageLayout title="404" subtitle="Page not found" className="prose">
-      <p>{"The page you're looking for doesn't exist."}</p>
-      <p>
-        <Link href="/">Go back home</Link>
+    <PageLayout
+      title="404"
+      subtitle="Page not found"
+      className="flex flex-col items-center justify-center gap-8"
+    >
+      <p className="text-center text-xl font-medium">
+        {"The page you're looking for doesn't exist."}
       </p>
+
+      <Button variant="primary" asChild>
+        <Link href="/">Go back home</Link>
+      </Button>
     </PageLayout>
   )
 }
