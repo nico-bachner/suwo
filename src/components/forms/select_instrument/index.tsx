9b296bfdb2fb67cb9948cdc5312b37452cefe1c8
Nico Bachner <git@nbac.me>
2025-03-19 23:29:08 +1100

    fix: nested instrument form

--- src/components/forms/select_instrument/index.tsx
+++ src/components/forms/select_instrument/index.tsx
@@ -1,7 +1,6 @@
 import { getInstruments } from '@/lib/db/instruments/get'
 import { getMember } from '@/lib/db/member/get_member'
 
-import { SettingsSection } from '../../ui/settings_section'
 import { SelectInstrumentForm } from './form'
 
 export const SelectInstrument = async () => {
@@ -9,11 +8,6 @@ export const SelectInstrument = async () => {
   const instruments = await getInstruments()
 
   return (
-    <SettingsSection
-      title="Instrument"
-      description="This is the instrument you usually play during SUWO rehearsals. If you play multiple instruments, choose the one you play most often."
-    >
-      <SelectInstrumentForm instrument={instrument} instruments={instruments} />
-    </SettingsSection>
+    <SelectInstrumentForm instrument={instrument} instruments={instruments} />
   )
 }
