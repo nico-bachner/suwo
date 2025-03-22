ec037ba86e62476cb0b4b40723ba0d528974f91c
Nico Bachner <git@nbac.me>
2025-03-22 11:47:38 +1100

    fix: loading skeleton

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,11 +1,5 @@
 import { PageLayout } from '@/components/ui/page_layout'
 
 export default function Loading() {
-  return (
-    <PageLayout
-      title="Loading"
-      subtitle="Please wait..."
-      className="prose flex flex-col gap-6"
-    />
-  )
+  return <PageLayout title="Loading" subtitle="Please wait..." />
 }
