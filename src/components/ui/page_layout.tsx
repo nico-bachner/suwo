72de48aa2c6222d0b811b045542685d74c8c3bcb
Nico Bachner <git@nbac.me>
2025-03-22 12:19:17 +1100

    fix: hide footer on large screens

--- src/components/ui/page_layout.tsx
+++ src/components/ui/page_layout.tsx
@@ -115,9 +115,9 @@ export const PageLayout = async ({
       <div className="mx-auto flex w-full max-w-screen-sm flex-1 flex-col gap-12 px-6 pt-2 pb-12 lg:pt-12">
         <main className={cn('flex-1', className)}>{children}</main>
 
-        <Separator.Root className="h-px w-full bg-gray-700" />
+        <Separator.Root className="h-px w-full bg-gray-700 lg:hidden" />
 
-        <section className="flex flex-col items-center gap-4">
+        <section className="flex flex-col items-center gap-4 lg:hidden">
           <p>Follow our socials</p>
 
           <div className="flex flex-row items-center gap-4">
