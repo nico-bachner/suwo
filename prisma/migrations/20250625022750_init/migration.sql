4be16a90c865c4ddc2d72cbc5e9646f05f7f7692
Nico Bachner <git@nbac.me>
2025-06-25 12:28:20 +1000

    fix: migration

--- prisma/migrations/20250625022750_init/migration.sql
+++ prisma/migrations/20250625022750_init/migration.sql
@@ -203,4 +203,3 @@ ALTER TABLE "MailingListRecipient" ADD CONSTRAINT "MailingListRecipient_user_id_
 
 -- AddForeignKey
 ALTER TABLE "Attendance" ADD CONSTRAINT "Attendance_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
