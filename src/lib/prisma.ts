30ec32badb747a296c6245b41c8e913dd38e9d42
Nico Bachner <git@nbac.me>
2025-07-19 06:26:04 +0200

    refactor: drop dotenv dependency

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,6 @@
 import { PrismaNeon } from '@prisma/adapter-neon'
-import dotenv from 'dotenv'
 
 import { PrismaClient } from '@/generated/prisma'
-
-dotenv.config()
 
 const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL })
 const prisma = new PrismaClient({ adapter })
