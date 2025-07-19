8b66d27dae240e8ac808d592960bfc1890119086
Nico Bachner <git@nbac.me>
2025-07-19 06:06:50 +0200

    refactor(date_manipulation): parse enum using zod

--- src/utils/date_manupulation/get_current_month.ts
+++ src/utils/date_manupulation/get_current_month.ts
@@ -1,35 +1,15 @@
-/* eslint-disable no-magic-numbers */
+import { z } from 'zod'
+
 import { Month } from './month'
 
 export const getCurrentMonth = () => {
-  const month = new Date().getMonth()
+  const { data: month, success } = z
+    .enum(Month)
+    .safeParse(new Date().getMonth())
 
-  switch (month) {
-    case 1:
-      return Month.January
-    case 2:
-      return Month.February
-    case 3:
-      return Month.March
-    case 4:
-      return Month.April
-    case 5:
-      return Month.May
-    case 6:
-      return Month.June
-    case 7:
-      return Month.July
-    case 8:
-      return Month.August
-    case 9:
-      return Month.September
-    case 10:
-      return Month.October
-    case 11:
-      return Month.November
-    case 12:
-      return Month.December
-    default:
-      throw new Error('Invalid month')
+  if (!success) {
+    throw new Error('Invalid month')
   }
+
+  return month
 }
