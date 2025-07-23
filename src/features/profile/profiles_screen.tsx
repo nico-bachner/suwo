6b8f23da213ac676dfc799297b25dd168c8ede3f
Nico Bachner <git@nbac.me>
2025-07-23 22:17:05 +0200

    feat: populate profile query cache

--- src/features/profile/profiles_screen.tsx
+++ src/features/profile/profiles_screen.tsx
@@ -1,16 +1,24 @@
 'use client'
 
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 
 import { queries } from '@/queries'
+import { queryKeys } from '@/routes'
 
 import { ProfilePreview, ProfilePreviewSkeleton } from './profile_preview'
 
 export const ProfilesScreen = () => {
+  const queryClient = useQueryClient()
   const { data: profiles, error, isPending } = useQuery(queries.PROFILES())
 
   if (error) {
     throw new Error(`Failed to fetch profiles: ${error.message}`)
+  }
+
+  if (profiles) {
+    profiles.forEach((profile) => {
+      queryClient.setQueryData(queryKeys.PROFILE(profile), profile)
+    })
   }
 
   return (
