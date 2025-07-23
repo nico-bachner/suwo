ad83bb789987ed3b7fa3b53fd8880a356581aff1
Nico Bachner <git@nbac.me>
2025-07-23 18:04:00 +0200

    fix: remove auth wall from attendance actions

--- src/app/api/attendance/[year]/[semester]/[week]/[user_id]/route.ts
+++ src/app/api/attendance/[year]/[semester]/[week]/[user_id]/route.ts
@@ -1,3 +1,31 @@
-import { logWeeklyAttendance } from '@/features/attendance/api_log_weekly_attendance'
+import { prettifyError } from 'zod'
 
-export const POST = logWeeklyAttendance
+import { LogWeeklyAttendanceCoerceValidator } from '@/features/attendance/validators'
+import { createResponse } from '@/utils/http/create_response'
+import { StatusCode } from '@/utils/http/status_code'
+import { APIRoute } from '@/utils/next_types'
+import { prisma } from '@/utils/prisma'
+
+export const POST: APIRoute = async (_, { params }) => {
+  const { data, error, success } = LogWeeklyAttendanceCoerceValidator.safeParse(
+    await params,
+  )
+
+  if (!success) {
+    return createResponse({
+      status: StatusCode.BadRequest,
+      error: prettifyError(error),
+    })
+  }
+
+  return createResponse({
+    status: StatusCode.OK,
+    data: await prisma.attendance.upsert({
+      where: {
+        user_id_year_semester_week: data,
+      },
+      create: data,
+      update: data,
+    }),
+  })
+}
--- src/app/api/attendance/[year]/[semester]/[week]/route.ts
+++ src/app/api/attendance/[year]/[semester]/[week]/route.ts
@@ -1,3 +1,27 @@
-import { getWeeklyAttendances } from '@/features/attendance/api_get_weekly_attendances'
+import { prettifyError } from 'zod'
 
-export const GET = getWeeklyAttendances
+import { WeeklyAttendancesCoerceValidator } from '@/features/attendance/validators'
+import { createResponse } from '@/utils/http/create_response'
+import { StatusCode } from '@/utils/http/status_code'
+import { APIRoute } from '@/utils/next_types'
+import { prisma } from '@/utils/prisma'
+
+export const GET: APIRoute = async (_, { params }) => {
+  const { data, error, success } = WeeklyAttendancesCoerceValidator.safeParse(
+    await params,
+  )
+
+  if (!success) {
+    return createResponse({
+      status: StatusCode.BadRequest,
+      error: prettifyError(error),
+    })
+  }
+
+  return createResponse({
+    status: StatusCode.OK,
+    data: await prisma.attendance.findMany({
+      where: data,
+    }),
+  })
+}
