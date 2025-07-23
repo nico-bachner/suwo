9ae315b9339a52f34a0902d5e5bb1a8214649d45
Nico Bachner <git@nbac.me>
2025-07-23 13:49:35 +0200

    fix: sort members

--- src/app/api/members/route.ts
+++ src/app/api/members/route.ts
@@ -1,3 +1,19 @@
-import { getProfiles } from '@/features/profile/profiles_api'
+import { createResponse } from '@/utils/http/create_response'
+import { StatusCode } from '@/utils/http/status_code'
+import { APIRoute } from '@/utils/next_types'
+import { prisma } from '@/utils/prisma'
 
-export const GET = getProfiles
+export const GET: APIRoute = async () => {
+  const profiles = await prisma.profile.findMany({
+    orderBy: [
+      {
+        given_name: 'asc',
+      },
+    ],
+  })
+
+  return createResponse({
+    status: StatusCode.OK,
+    data: profiles,
+  })
+}
--- src/app/api/members/update-instrument/route.ts
+++ src/app/api/members/update-instrument/route.ts
@@ -1,1 +1,43 @@
-export * from '@/features/profile/update_instrument_route'
+import { prettifyError } from 'zod'
+
+import { getSession } from '@/features/auth/session/server/get_session'
+import { UpdateInstrumentValidator } from '@/features/profile/update_instrument_validator'
+import { createResponse } from '@/utils/http/create_response'
+import { StatusCode } from '@/utils/http/status_code'
+import { prisma } from '@/utils/prisma'
+
+export const POST = async (request: Request) => {
+  const session = await getSession()
+
+  if (!session) {
+    return createResponse({
+      status: StatusCode.Unauthorized,
+      error: 'Unauthorized',
+    })
+  }
+
+  const { data, error, success } = UpdateInstrumentValidator.safeParse(
+    await request.json(),
+  )
+
+  if (!success) {
+    return createResponse({
+      status: StatusCode.BadRequest,
+      error: prettifyError(error),
+    })
+  }
+
+  const updatedUser = await prisma.profile.update({
+    where: {
+      user_id: session.user_id,
+    },
+    data: {
+      instrument_name: data.instrument_name,
+    },
+  })
+
+  return createResponse({
+    status: StatusCode.OK,
+    data: updatedUser,
+  })
+}
