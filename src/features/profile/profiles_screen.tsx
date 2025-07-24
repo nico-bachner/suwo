77b793a1a3dc68d517f468700186f8fa073ef76f
Nico Bachner <git@nbac.me>
2025-07-24 08:59:09 +0200

    feat: search profiles

--- src/features/profile/profiles_screen.tsx
+++ src/features/profile/profiles_screen.tsx
@@ -1,15 +1,19 @@
 'use client'
 
 import { useQuery, useQueryClient } from '@tanstack/react-query'
+import { useState } from 'react'
 
+import { TextInput } from '@/design_system/text_input'
 import { queries } from '@/queries'
 import { queryKeys } from '@/routes'
+import { search } from '@/utils/search'
 
 import { ProfilePreview, ProfilePreviewSkeleton } from './profile_preview'
 
 export const ProfilesScreen = () => {
   const queryClient = useQueryClient()
   const { data: profiles, error, isPending } = useQuery(queries.PROFILES())
+  const [query, setQuery] = useState('')
 
   if (error) {
     throw new Error(`Failed to fetch profiles: ${error.message}`)
@@ -25,12 +29,27 @@ export const ProfilesScreen = () => {
     <div className="prose mx-auto max-w-screen-sm px-4 py-8">
       <h1>Members</h1>
 
+      <TextInput
+        name="search"
+        label="Search"
+        type="search"
+        onChange={({ target }) => {
+          setQuery(target.value)
+        }}
+        placeholder="Search by name or instrument"
+        className="mb-6"
+      />
+
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
         {isPending
           ? Array.from({ length: 20 }).map((_, index) => (
               <ProfilePreviewSkeleton key={index} />
             ))
-          : profiles.map((profile) => (
+          : search({
+              data: profiles,
+              keys: ['given_name', 'family_name', 'instrument_name'],
+              query,
+            }).map((profile) => (
               <ProfilePreview key={profile.user_id} {...profile} />
             ))}
       </div>
