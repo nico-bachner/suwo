41cf9ed12c0a7fcba9a5b2a31a55fd2980812ebd
Nico Bachner <git@nbac.me>
2025-07-30 13:21:29 +0200

    refactor: instruments api route

--- src/app/api/instruments/route.ts
+++ src/app/api/instruments/route.ts
@@ -2,10 +2,14 @@ import { createResponse } from '@/utils/http/create_response'
 import { StatusCode } from '@/utils/http/status_code'
 import { prisma } from '@/utils/prisma'
 
-export const GET = async () =>
-  createResponse({
+export const GET = async () => {
+  const instruments = await prisma.instrument.findMany()
+  const sortedInstruments = instruments.toSorted((a, b) =>
+    a.name.localeCompare(b.name),
+  )
+
+  return createResponse({
     status: StatusCode.OK,
-    data: (await prisma.instrument.findMany()).toSorted((a, b) =>
-      a.name.localeCompare(b.name),
-    ),
+    data: sortedInstruments,
   })
+}
