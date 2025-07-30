dfd3ed63fe8da51052a8b706bfc363b42f1b984d
Nico Bachner <git@nbac.me>
2025-07-30 13:20:06 +0200

    fix: require authentication to view equipment

--- src/app/api/equipment/route.ts
+++ src/app/api/equipment/route.ts
@@ -1,10 +1,21 @@
+import { getSession } from '@/features/auth/session/get_session'
 import { createResponse } from '@/utils/http/create_response'
 import { StatusCode } from '@/utils/http/status_code'
 import { APIRoute } from '@/utils/next_types'
 import { prisma } from '@/utils/prisma'
 
-export const GET: APIRoute = async () =>
-  createResponse({
+export const GET: APIRoute = async () => {
+  const session = await getSession()
+
+  if (!session) {
+    return createResponse({
+      status: StatusCode.Unauthorized,
+      error: 'Unauthorized',
+    })
+  }
+
+  return createResponse({
     status: StatusCode.OK,
     data: await prisma.equipment.findMany(),
   })
+}
