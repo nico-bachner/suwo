c0d2f13d7db19766913b9d9e557ae82cece7b889
Nico Bachner <git@nbac.me>
2025-08-08 16:18:57 +1000

    fix: password input autocomplete

--- src/design_system/input/password_input.tsx
+++ src/design_system/input/password_input.tsx
@@ -13,7 +13,7 @@ export const PasswordInput = ({
   className,
   ...props
 }: ComponentProps<typeof Input>) => (
-  <Root {...props}>
+  <Root>
     <div
       className={cn(
         'bg-neutral-7 text-neutral-1 border-neutral-4 flex items-center rounded-md border',
@@ -24,6 +24,7 @@ export const PasswordInput = ({
         suppressHydrationWarning
         placeholder='e.g. "I<3SUWO25!"'
         className="selection:bg-neutral-4 selection:text-neutral-1 flex-1 pl-3 outline-none"
+        {...props}
       />
       <Toggle className="focus-visible:ring-primary-3 flex items-center justify-center rounded-md outline-none focus-visible:ring-2">
         <Icon
