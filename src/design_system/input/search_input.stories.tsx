2bb81893749c34a405513ad27dc9bc151c3d2712
Nico Bachner <git@nbac.me>
2025-08-08 17:33:49 +1000

    fix: storybook deployment

--- src/design_system/input/search_input.stories.tsx
+++ src/design_system/input/search_input.stories.tsx
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from '@storybook/nextjs'
 import { SearchInput } from './search_input'
 
 const meta: Meta<typeof SearchInput> = {
-  title: 'Components/Input/Password',
+  title: 'Components/Input/Search',
   component: SearchInput,
 }
 
