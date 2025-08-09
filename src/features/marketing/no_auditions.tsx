652a9917745d70ee397a31e3e89d831aa0bb4bc9
Nico Bachner <git@nbac.me>
2025-08-09 20:13:20 +1000

    feat: more animations for no audition

--- src/features/marketing/no_auditions.tsx
+++ src/features/marketing/no_auditions.tsx
@@ -7,22 +7,51 @@ import { cn } from '@/utils/cn'
 
 import no_auditions from './no_auditions.png'
 
+enum Position {
+  TOP_LEFT = 'top-left',
+  TOP_RIGHT = 'top-right',
+  BOTTOM_LEFT = 'bottom-left',
+  BOTTOM_RIGHT = 'bottom-right',
+}
+
+const getPosition = (position: Position) => {
+  switch (position) {
+    case Position.BOTTOM_LEFT:
+      return 'translate-x-0 translate-y-0 rotate-y-0'
+    case Position.BOTTOM_RIGHT:
+      return 'translate-x-[100%] translate-y-0 rotate-y-180 sm:translate-x-[200%] lg:translate-x-[300%]'
+    case Position.TOP_LEFT:
+      return 'translate-x-0 translate-y-[-300%] sm:translate-y-[-200%] lg:translate-y-[-100%] rotate-y-0'
+    case Position.TOP_RIGHT:
+      return 'translate-x-[100%] translate-y-[-300%] sm:translate-y-[-200%] lg:translate-y-[-100%] rotate-y-180 sm:translate-x-[200%] lg:translate-x-[300%]'
+  }
+}
+
 export const NoAuditions = () => {
-  const [flipped, setFlipped] = useState(false)
+  const [position, setPosition] = useState<Position>(Position.BOTTOM_LEFT)
 
   return (
-    <div className="pointer-events-none fixed inset-0 z-50 flex w-full flex-row items-end">
+    <div className="pointer-events-none fixed inset-0 z-50 flex h-full w-full flex-row items-end">
       <Image
         src={no_auditions}
         alt="No Auditions"
         className={cn(
-          'pointer-events-auto w-1/2 transition-transform duration-1000 sm:w-1/4',
-          flipped
-            ? 'translate-x-[100%] rotate-y-180 sm:translate-x-[300%]'
-            : 'translate-x-0 rotate-y-0',
+          'pointer-events-auto h-1/4 w-1/2 transition-transform duration-1000 sm:h-1/3 sm:w-1/3 lg:h-1/2 lg:w-1/4',
+          getPosition(position),
         )}
         onClick={() => {
-          setFlipped(!flipped)
+          setPosition((prev) => {
+            switch (prev) {
+              case Position.BOTTOM_LEFT:
+                return Position.BOTTOM_RIGHT
+              case Position.BOTTOM_RIGHT:
+                return Position.TOP_LEFT
+              case Position.TOP_LEFT:
+                return Position.TOP_RIGHT
+              case Position.TOP_RIGHT:
+                return Position.BOTTOM_LEFT
+            }
+          })
         }}
       />
     </div>
