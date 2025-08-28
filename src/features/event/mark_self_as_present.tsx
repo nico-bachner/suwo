477c6a5922bfbe68c69e3b5db9f0e87536307f8d
Nico Bachner <git@nbac.me>
2025-08-28 11:16:36 +1000

    feat: mark self as present on user home

--- src/features/event/mark_self_as_present.tsx
+++ src/features/event/mark_self_as_present.tsx
@@ -2,25 +2,25 @@ import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 
 import { Button } from '@/design_system/button'
 import { EventDTO } from '@/lib/dtos/event_dto_validator'
-import { SessionDTO } from '@/lib/dtos/session_dto_validator'
+import { UserDTO } from '@/lib/dtos/user_dto_validator'
 import { mutations } from '@/lib/mutations'
 import { queries } from '@/lib/queries'
 
 type MarkSelfAsPresentProps = {
-  session: SessionDTO
+  user_id: UserDTO['id']
   event: EventDTO
   className?: string
 }
 
 export const MarkSelfAsPresent = ({
-  session,
+  user_id,
   event,
   className,
 }: MarkSelfAsPresentProps) => {
   const queryClient = useQueryClient()
-  const { data: user } = useQuery(queries.USER(session.user_id))
+  const { data: user } = useQuery(queries.USER(user_id))
   const { mutate: updateUser } = useMutation(
-    mutations.USER(queryClient, session.user_id),
+    mutations.USER(queryClient, user_id),
   )
 
   if (!user) {
