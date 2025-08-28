909df9a98725d82477426000db6c0b4d3472c114
Nico Bachner <git@nbac.me>
2025-08-28 15:48:50 +1000

    chore: upgrade dependencies

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -2,5 +2,8 @@ import { PrismaNeon } from '@prisma/adapter-neon'
 
 import { PrismaClient } from '@/generated/prisma'
 
-const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL })
-export const prisma = new PrismaClient({ adapter })
+export const prisma = new PrismaClient({
+  adapter: new PrismaNeon({
+    connectionString: process.env.DATABASE_URL,
+  }),
+})
