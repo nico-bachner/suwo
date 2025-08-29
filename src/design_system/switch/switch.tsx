fe63e42327319d33c2168325446214b14bdc4324
Nico Bachner <git@nbac.me>
2025-08-29 13:46:21 +1000

    fix: switch

--- src/design_system/switch/switch.tsx
+++ src/design_system/switch/switch.tsx
@@ -9,10 +9,22 @@ export const Switch = ({ name, label, className, ...props }: SwitchProps) => (
     <Root
       id={name}
       name={name}
-      className="data-[state=unchecked]:bg-negative-3 data-[state=checked]:bg-positive-3 data-[state=unchecked]:inset-shadow-negative-1 data-[state=checked]:inset-shadow-positive-1 flex h-8 w-20 cursor-pointer items-center rounded-full p-1 inset-shadow-xs transition-colors outline-none"
+      className={cn(
+        'flex h-8 w-20 cursor-pointer items-center rounded-full border p-1 outline-offset-2 focus-visible:outline-4',
+        'transition-colors duration-300',
+        'data-[state=checked]:bg-positive-3 data-[state=checked]:border-positive-2',
+        'data-[state=unchecked]:bg-negative-3 data-[state=unchecked]:border-negative-2',
+      )}
       {...props}
     >
-      <Thumb className="bg-neutral-1 h-full w-10 rounded-full transition-transform data-[state=checked]:translate-x-[32px]" />
+      <Thumb
+        className={cn(
+          'bg-neutral-1 h-full w-10 rounded-full',
+          'transition-transform duration-300',
+          'data-[state=unchecked]:translate-x-0',
+          'data-[state=checked]:translate-x-[calc(100%-10px)]',
+        )}
+      />
     </Root>
 
     <label htmlFor={name} className="text-neutral-2 font-bold select-none">
